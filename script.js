8c8b32c12ec6cbb8c26b60a797d14c4386969ee5
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-09 23:12:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+() => { 
+  cy.get('.col-lg-8 .block-heading').should('have.length', 1).and('have.css', 'text-align', 'center'); 
+  cy.get('.col-lg-8 .block-heading p').should('have.css', 'text-align', 'center'); 
+}
